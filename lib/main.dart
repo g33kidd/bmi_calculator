669ee22e3020b935b484716c660a841bd66c15a4
Joshua Kidd <kidd.josh.343@gmail.com>
2019-05-24 11:11:05 -0500

    All the other stuff

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,228 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(App());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+const Color blue = Color(0xFF1A4B76);
+const Color yellow = Color(0xFFFFBD2A);
+
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: "BMI Calculator",
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primaryTextTheme: TextTheme(
+          display1: TextStyle(
+            color: blue,
+          ),
+          title: TextStyle(
+            color: yellow,
+          ),
+        ),
+        appBarTheme: AppBarTheme(
+          textTheme: TextTheme(display1: TextStyle(color: yellow)),
+          color: blue,
+          actionsIconTheme: IconThemeData(
+            color: yellow,
+          ),
+        ),
+        textTheme: TextTheme(
+          display1: TextStyle(color: blue),
+        ),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomeScreen extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomeScreenState extends State<HomeScreen> {
+  double bmi = 0.0;
+  double _opacity = 0.0;
 
-  void _incrementCounter() {
+  bool showBMI = false;
+
+  int heightFeet = 5;
+  int heightInches = 9;
+  int totalHeight = 120;
+  int weight = 180;
+
+  void calculate() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      totalHeight = heightInches + (heightFeet * 12);
+      bmi = 703 * (weight / pow(totalHeight, 2));
+      showBMI = true;
     });
+  }
+
+  String classification() {
+    var result;
+    if (bmi < 18.5) result = "Underweight";
+    if (bmi > 18.5 && bmi < 25) result = "Normal";
+    if (bmi >= 25 && bmi < 30) result = "Overweight";
+    if (bmi > 30) result = "Obese";
+    return result;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("BMI Calculator"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.info_outline),
+            onPressed: () {
+              print("Information about BMI.");
+            },
+          )
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: GestureDetector(
+        onTap: () {
+          setState(() {
+            showBMI = false;
+          });
+        },
+        child: Stack(
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            AnimatedOpacity(
+              opacity: showBMI ? 1.0 : 0.0,
+              curve: Curves.ease,
+              duration: Duration(milliseconds: 500),
+              child: Container(
+                width: double.infinity,
+                height: double.infinity,
+                color: Colors.black54,
+                child: SafeArea(
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Container(
+                      width: MediaQuery.of(context).size.width / 1.2,
+                      height: MediaQuery.of(context).size.height / 2,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(4.0),
+                      ),
+                      child: FittedBox(
+                        child: Text(bmi.toString()),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            AnimatedOpacity(
+              opacity: showBMI ? 0.0 : 1.0,
+              duration: Duration(milliseconds: 450),
+              curve: Curves.ease,
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "We need your height and weight in order to determine your BMI score.",
+                      style:
+                          TextStyle(fontSize: 18.0, height: 1.2, color: blue),
+                    ),
+                    SizedBox(height: 16.0),
+                    Text(
+                      "WEIGHT",
+                      style: TextStyle(
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                        color: blue,
+                      ),
+                    ),
+                    SizedBox(height: 8.0),
+                    TextField(
+                      onChanged: (data) {
+                        setState(() {
+                          weight = int.parse(data);
+                        });
+                      },
+                      decoration: InputDecoration(
+                        hintText: "in pounds (lb)",
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    SizedBox(height: 24.0),
+                    Text(
+                      "HEIGHT",
+                      style: TextStyle(
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                        color: blue,
+                      ),
+                    ),
+                    SizedBox(height: 8.0),
+                    Row(
+                      children: <Widget>[
+                        Flexible(
+                          flex: 1,
+                          child: TextField(
+                            onChanged: (data) {
+                              setState(() {
+                                heightFeet = int.parse(data);
+                              });
+                            },
+                            decoration: InputDecoration(
+                              hintText: "ft",
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 12.0),
+                        Flexible(
+                          flex: 2,
+                          child: TextField(
+                            onChanged: (data) {
+                              setState(() {
+                                heightInches = int.parse(data);
+                              });
+                            },
+                            decoration: InputDecoration(
+                              hintText: "in",
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 36.0),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 60,
+                      child: (!showBMI)
+                          ? FlatButton(
+                              child: Text(
+                                "CALCULATE MY BMI",
+                                style: TextStyle(fontSize: 24.0),
+                              ),
+                              onPressed: () {
+                                calculate();
+                              },
+                              color: yellow,
+                              textColor: Colors.white,
+                              splashColor: blue,
+                            )
+                          : Offstage(),
+                    )
+                  ],
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
